5882f9835800a78e1bd9462f3cfd18b7dc265478
van-riper <64651989+van-riper@users.noreply.github.com>
2026-04-29 20:25:57 -0400

    run train_model thru main script

--- signsight/__main__.py
+++ signsight/__main__.py
@@ -1,15 +1,17 @@
 import sys
 
+# Must use python 3.12
 if sys.version_info < (3, 12) or sys.version_info >= (3, 13):
-    print("error: signsight requires python 3.12")
-    print(
-        f"detected version: {sys.version_info.major}.{sys.version_info.minor}"
-    )
+    print(f"error: python {sys.version} in use, python 3.12 is required")
     sys.exit(1)
+
+from signsight.train import train_model
 
 
 def main() -> None:
-    pass
+    """Entry point for the SignSight CLI."""
+
+    train_model()
 
 
 if __name__ == "__main__":
