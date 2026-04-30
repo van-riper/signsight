3f1023937bf5db83f20755ab5ac37b030c81d2f8
van-riper <64651989+van-riper@users.noreply.github.com>
2026-04-29 20:21:40 -0400

    calculate validation statistics

--- signsight/train.py
+++ signsight/train.py
@@ -53,10 +53,45 @@ def train_model() -> None:
             optimizer.step()
             train_loss += loss.item()
 
+        val_loss, val_accuracy = _validate(
+            model, val_loader, criterion, device, len(val_set)
+        )
+
         print(
             f"Epoch {epoch + 1}/{EPOCH_COUNT} "
             f"| Train Loss: {train_loss / len(train_loader):.4f} "
+            f"| Val Loss: {val_loss:.4f} "
+            f"| Val Accuracy: {val_accuracy:.2f}%"
         )
 
     torch.save(model.state_dict(), MODEL_PATH)
     print(f"Model saved to {MODEL_PATH}")
+
+
+def _validate(
+    model: torch.nn.Module,
+    loader: DataLoader,
+    criterion: torch.nn.CrossEntropyLoss,
+    device: torch.device,
+    dataset_size: int,
+) -> tuple[float, float]:
+    """Run validation loop and return average loss and accuracy."""
+
+    loss_total: float = 0.0
+    correct_count: int = 0
+
+    model.eval()
+
+    # Disable gradient calculation since this is just calculating the losses
+    with torch.no_grad():
+        for images, labels in loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+
+            loss_total += criterion(outputs, labels).item()
+            correct_count += (outputs.argmax(1) == labels).sum().item()
+
+    loss_average: float = loss_total / len(loader)
+    accuracy: float = 100 * correct_count / dataset_size
+
+    return loss_average, accuracy
