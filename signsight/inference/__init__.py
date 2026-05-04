3444b052980d7212173af6ad1587d84c8271d236
Finn van Riper <64651989+van-riper@users.noreply.github.com>
2026-05-04 10:54:20 -0400

    chore: display module exports

--- signsight/inference/__init__.py
+++ signsight/inference/__init__.py
@@ -2,4 +2,5 @@
 
 from .capture import close_camera, open_camera, read_frame
 from .detect import create_hand_detector, detect_hand
+from .display import draw_landmarks, draw_no_hand_message, draw_prediction
 from .predict import load_predictor, predict
